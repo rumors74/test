7db88402fc335a36331bc3055ee78d474fe879a4
dzyang <1251892507@qq.com>
2021-07-20 01:07:26 +0800

    新增了Acc文件

---
+++ untitled12/src/main/java/org/example/Acc.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import jdk.vm.ci.hotspot.HotSpotVMConfigAccess;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Rumors74
+ * Date: 2021-07-20- 1:06
+ */
+public class Acc {
+    public static void main(String[] args) {
+        System.out.println("jjj");
+    }
+}
