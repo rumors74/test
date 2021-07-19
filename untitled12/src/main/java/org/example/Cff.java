818889e0e0fd6d54661d3cec14da7602428f2c68
dzyang <1251892507@qq.com>
2021-07-20 00:58:30 +0800

    新增了Cff文件

---
+++ untitled12/src/main/java/org/example/Cff.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Rumors74
+ * Date: 2021-07-20- 0:57
+ */
+public class Cff {
+    public static void main(String[] args) {
+        System.out.println("hellp");
+    }
+}
